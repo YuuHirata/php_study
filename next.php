c196bf65ec376ab861cdaf051945560c8073d6dd
y0u <y0u@localhost.localdomain>
2020-03-17 14:07:37 +0900

    構文

---
+++ next.php
@@ -0,0 +1,27 @@
+<?php
+$car = new Car();
+
+class Car{
+public $aa;
+
+}
+
+if (condition) {
+    # code...
+}
+
+if (condition) {
+    # code...
+} else {
+    # code...
+}
+
+switch ($variable) {
+    case 'value':
+        # code...
+        break;
+    
+    default:
+        # code...
+        break;
+}
